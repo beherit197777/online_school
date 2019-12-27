a5a8846f492696130944bb4db411bedf2f24c290
beherit197777 <beherit197777@gmail.com>
2019-12-27 10:29:56 +0200

    add files

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,27 @@ module SApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
+    config.exceptions_app = self.routes
+    config.active_record.belongs_to_required_by_default = true
+
+    config.generators do |g|
+      g.helper      false
+      g.javascripts false
+      g.stylesheets false
+      g.decorator   false
+
+      g.template_engine :slim
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false
+    end
   end
 end
